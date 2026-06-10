6e5a92ab00f5dbeb7e3e37f20ce4169845fac10d
iurygandolfo01-eng <iurygandolfo01@gmail.com>
2026-06-09 22:44:38 -0300

    Delete dist directory

--- dist/server/assets/_tanstack-start-manifest_v-Dj7bGvJf.js
+++
@@ -1,4 +0,0 @@
-const tsrStartManifest = () => ({ routes: { __root__: { filePath: "C:/Users/hisokaxzn/Downloads/ggzimports-main/src/routes/__root.tsx", children: ["/", "/produto/$slug"], preloads: ["/assets/index-fXHRwivG.js"], scripts: [{ attrs: { type: "module", async: true, src: "/assets/index-fXHRwivG.js" } }] }, "/": { filePath: "C:/Users/hisokaxzn/Downloads/ggzimports-main/src/routes/index.tsx", children: void 0, preloads: ["/assets/index-Q691V8JH.js", "/assets/truck-B3WkLYxE.js"] }, "/produto/$slug": { filePath: "C:/Users/hisokaxzn/Downloads/ggzimports-main/src/routes/produto.$slug.tsx", children: void 0, preloads: ["/assets/produto._slug-CQen-9a_.js", "/assets/truck-B3WkLYxE.js"] } } });
-export {
-  tsrStartManifest
-};
